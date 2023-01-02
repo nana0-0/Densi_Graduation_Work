a9fedf0947959fd679a2ffdec99577c2c7a699a8
nanai <51441651+nana0-0@users.noreply.github.com>
2023-01-02 21:02:53 +0900

    修正

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
     <>
       <Head>
         <title>ボードゲーム『宇宙人はダレ？』公式サイト</title>
-        <meta name="description" content="ボードゲーム『宇宙人はダレ？』遊びで中１ギャップを未然に防ぐ卒業制作プロジェクト" />
+        <meta name="description" content="ボードゲーム『宇宙人はダレ？』遊びで中１ギャップを未然に防ぐ卒業制作プロジェクト。日本電子専門学校 Webデザイン科 卒業・進級制作展2022にて展示します。" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
